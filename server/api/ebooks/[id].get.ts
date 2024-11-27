6796303d0592629ec1e6a2a3c4d5917185190c4d
Tran Viet Dung <tvd1819@gmail.com>
2024-11-27 07:12:49 +0700

    edit opds download

--- server/api/ebooks/[id].get.ts
+++ server/api/ebooks/[id].get.ts
@@ -1,9 +1,33 @@
 import { bot } from '../../utils/bot'
+
 export default defineEventHandler(async event => {
-  const { id } = event.context.params || {}
-  const config = useRuntimeConfig()
-  const ebookFile = await bot.api.getFile(id)
-  const filePath = ebookFile.file_path
-  const fileUrl = `https://api.telegram.org/file/bot${config.telegramBotToken}/${filePath}`
-  return sendRedirect(event, fileUrl)
+  try {
+    const { id } = event.context.params || {}
+    const config = useRuntimeConfig()
+    const db = useDrizzle()
+    const fileOnServer = await bot.api.getFile(id)
+    const ebookFile = await db.query.tlgFiles.findFirst({
+      where: eq(tables.tlgFiles.id, id)
+    })
+    const filePath = fileOnServer.file_path
+    const fileUrl = `https://api.telegram.org/file/bot${config.telegramBotToken}/${filePath}`
+
+    // Fetch file content
+    const response = await fetch(fileUrl)
+    const fileBuffer = await response.arrayBuffer()
+    let fileName = ebookFile?.name?.replace(/[^a-zA-Z0-9.-\s]/g, '')
+    fileName = fileName?.replace(/\.(epub|pdf|mobi|azw3)$/i, '')
+    // Set appropriate headers
+    setHeader(event, 'Content-Type', 'application/octet-stream')
+    setHeader(event, 'Content-Disposition', `attachment; filename="${fileName}"`)
+    
+    // Return file buffer directly
+    return Buffer.from(fileBuffer)
+  } catch (error) {
+    console.error('Error downloading file:', error)
+    throw createError({
+      statusCode: 500,
+      message: 'Error downloading file'
+    })
+  }
 })
