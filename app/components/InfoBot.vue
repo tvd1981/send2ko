1eded56b25a316aae3363bf30995f9915e20c0d7
Tran Viet Dung <tvd1819@gmail.com>
2024-12-01 13:29:00 +0700

    update index page

--- app/components/InfoBot.vue
+++ app/components/InfoBot.vue
@@ -3,12 +3,14 @@
       <h1 :style="{ color }">
           Send to Koreader Bot 👋
       </h1>
-      <hr>
+      <UDivider label="Bot Info" type="solid" class="my-4"/>
       <img class="qr" src="/images/qr-bot.png" alt="qr telegram bot" width="200">
       <br>
       <a href="https://t.me/send2koreader_bot" target="_blank">
         <span :style="{ color }">https://t.me/send2koreader_bot</span>
       </a>
+      <UDivider icon="i-simple-icons-github" type="solid" class="my-4"/>
+      <a href="https://github.com/tvd1981/send2ko" target="_blank"><span :style="{ color }">https://github.com/tvd1981/send2ko</span></a>
     </div>
   </template>
 
@@ -25,9 +27,6 @@
   </script>
 
   <style scoped>
-  hr{
-    margin: 20px 0;
-  }
   .qr{
     display: block;
     margin: 0 auto;
