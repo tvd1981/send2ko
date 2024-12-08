42940e5ca5c038155b43ad6fbeec26164313b242
Tran Viet Dung <tvd1819@gmail.com>
2024-12-09 00:31:38 +0700

    edit epub generator

--- server/utils/epub-generator.ts
+++ server/utils/epub-generator.ts
@@ -55,83 +55,73 @@ export class EpubGenerator {
   }
 
   private generateContentOpf(): string {
-    const manifest = [
-      { "@_id": "ncx", "@_href": "toc.ncx", "@_media-type": "application/x-dtbncx+xml" },
-      ...this.chapters.map(chapter => ({
-        "@_id": chapter.id,
-        "@_href": `${chapter.id}.xhtml`,
-        "@_media-type": "application/xhtml+xml"
-      }))
-    ];
+    const manifest = [];
 
+    // Add cover first if it exists
     if (this.metadata.cover) {
       const coverExt = this.metadata.cover.mimeType.split('/')[1];
-      console.log('Cover metadata:', {
-        id: this.metadata.cover.id,
-        mimeType: this.metadata.cover.mimeType,
-        dataLength: this.metadata.cover.data?.length
-      });
       manifest.push({
         "@_id": this.metadata.cover.id,
         "@_href": `images/${this.metadata.cover.id}.${coverExt}`,
         "@_media-type": this.metadata.cover.mimeType
       });
+      // Add cover-html
+      manifest.push({
+        "@_id": "cover-html",
+        "@_href": "cover.xhtml",
+        "@_media-type": "application/xhtml+xml"
+      });
     }
 
-    const spine = ["ncx", ...this.chapters.map(chapter => chapter.id)].map(id => ({
-      "@_idref": id
-    }));
+    // Add content
+    manifest.push({
+      "@_id": "content",
+      "@_href": "content.xhtml",
+      "@_media-type": "application/xhtml+xml"
+    });
+
+    // Build spine items - start with cover if exists
+    const spineItems = [];
+    if (this.metadata.cover) {
+      spineItems.push({ "@_idref": "cover-html" });
+    }
+    // Add content to spine
+    spineItems.push({ "@_idref": "content" });
 
     const content = {
       package: {
         "@_version": "2.0",
         "@_xmlns": "http://www.idpf.org/2007/opf",
+        "@_unique-identifier": "BookId",
         metadata: {
+          "@_xmlns:dc": "http://purl.org/dc/elements/1.1/",
+          "@_xmlns:opf": "http://www.idpf.org/2007/opf",
           "dc:title": this.metadata.title,
-          "dc:creator": this.metadata.author,
+          "dc:creator": { "@_opf:role": "aut", "#text": this.metadata.author },
           "dc:language": this.metadata.language,
-          "dc:identifier": this.metadata.identifier,
+          "dc:identifier": { "@_id": "BookId", "#text": this.metadata.identifier },
           ...(this.metadata.description && { "dc:description": this.metadata.description }),
-          "@_xmlns:dc": "http://purl.org/dc/elements/1.1/"
+          ...(this.metadata.cover && { meta: { "@_name": "cover", "@_content": this.metadata.cover.id } })
         },
         manifest: { item: manifest },
-        spine: { itemref: spine }
+        spine: { itemref: spineItems }
       }
     };
 
-    console.log('Generated content.opf:', JSON.stringify(content, null, 2));
     return this.xmlBuilder.build(content);
   }
 
-  private generateTocNcx(): string {
-    const navPoints = this.chapters.map((chapter, index) => ({
-      "@_id": `navPoint-${index + 1}`,
-      "@_playOrder": index + 1,
-      navLabel: { text: chapter.title },
-      content: { "@_src": `${chapter.id}.xhtml` }
-    }));
-
-    const toc = {
-      ncx: {
-        "@_xmlns": "http://www.daisy.org/z3986/2005/ncx/",
-        "@_version": "2005-1",
-        head: {
-          meta: { "@_name": "dtb:uid", "@_content": this.metadata.identifier }
-        },
-        docTitle: { text: this.metadata.title },
-        navMap: { navPoint: navPoints }
-      }
-    };
-
-    return this.xmlBuilder.build(toc);
-  }
-
-  private generateChapterXhtml(chapter: EpubChapter): string {
+  private generateContentXhtml(): string {
+    const chapter = this.chapters[0]; // We only use the first chapter
     return `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <title>${chapter.title}</title>
+  <style type="text/css">
+    body { margin: 1em; line-height: 1.6; }
+    h1 { text-align: center; margin: 1em 0; }
+  </style>
 </head>
 <body>
   <h1>${chapter.title}</h1>
@@ -140,32 +130,49 @@ export class EpubGenerator {
 </html>`;
   }
 
+  private generateCoverHtml(): string {
+    if (!this.metadata.cover) return '';
+    const coverExt = this.metadata.cover.mimeType.split('/')[1];
+    return `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Cover</title>
+  <style type="text/css">
+    body { margin: 0; padding: 0; text-align: center; }
+    img { max-width: 100%; max-height: 100vh; margin: 0; padding: 0; }
+  </style>
+</head>
+<body>
+  <div>
+    <img src="images/${this.metadata.cover.id}.${coverExt}" alt="Cover"/>
+  </div>
+</body>
+</html>`;
+  }
+
   async generate(): Promise<Uint8Array> {
     try {
+      // Create files object with mimetype first
       const files: Record<string, Uint8Array> = {
-        'mimetype': strToU8('application/epub+zip'),
+        'mimetype': strToU8('application/epub+zip')
+      };
+
+      // Add other files
+      Object.assign(files, {
         'META-INF/container.xml': strToU8(this.generateContainerXml()),
         'OEBPS/content.opf': strToU8(this.generateContentOpf()),
-        'OEBPS/toc.ncx': strToU8(this.generateTocNcx()),
-      };
-      // console.log('add chapter', new Date().toISOString())
-      // Add chapters
-      for (const chapter of this.chapters) {
-        files[`OEBPS/${chapter.id}.xhtml`] = strToU8(this.generateChapterXhtml(chapter));
-      }
+      });
+
       // Add cover if exists
       if (this.metadata.cover) {
-        const coverExt = this.metadata.cover.mimeType.split('/')[1]; // Get extension from mime type
+        const coverExt = this.metadata.cover.mimeType.split('/')[1];
+        files['OEBPS/cover.xhtml'] = strToU8(this.generateCoverHtml());
         files[`OEBPS/images/${this.metadata.cover.id}.${coverExt}`] = this.metadata.cover.data;
       }
-      // console.log('zip', new Date().toISOString())
-      // // Debug logging
-      // Object.entries(files).forEach(([path, data]) => {
-      //   console.log('File path:', path);
-      //   console.log('Data type:', data ? data.constructor.name : 'undefined');
-      //   console.log('Data length:', data ? data.length : 'N/A');
-      //   console.log('---');
-      // });
+
+      // Add content
+      files['OEBPS/content.xhtml'] = strToU8(this.generateContentXhtml());
 
       return new Promise<Uint8Array>((resolve, reject) => {
         try {
